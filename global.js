a65e1334eb2ff63afde727d1bb03b060cd4d9c67
Pelle Stenild Coltau <pelle@tlt.local>
2016-03-07 21:42:28 +0400

    Now using containers when adding images and shapes.

--- global.js
+++ global.js
@@ -1,28 +1,30 @@
 SCREEN_WIDTH = 1024;
 SCREEN_HEIGHT = 768;
 
-function line(shape, color, x1, y1, x2, y2) {
-  shape.graphics.setStrokeStyle(1).beginStroke(color).moveTo(x1, y1).lineTo(x2, y2).endStroke();
+function line(graphics, color, x1, y1, x2, y2) {
+  graphics.setStrokeStyle(1).beginStroke(color).moveTo(x1, y1).lineTo(x2, y2).endStroke();
 }
 
-function bar(shape, color, x1, y1, x2, y2) {
+function bar(graphics, color, x1, y1, x2, y2) {
   var startX = Math.min(x1, x2);
   var endX = Math.max(x1, x2);
   var startY = Math.min(y1, y2);
   var endY = Math.max(y1, y2);
-  shape.graphics.setStrokeStyle(1).beginFill(color).drawRect(startX, startY, endX - startX, endY - startY).endFill();
+  graphics.setStrokeStyle(1).beginFill(color).drawRect(startX, startY, endX - startX, endY - startY).endFill();
 }
 
-function putPixel(shape, color, x, y) {
-  line(shape, color, x, y, x+1, y);
+function putPixel(graphics, color, x, y) {
+  line(graphics, color, x, y, x+1, y);
 }
 
-function outTextXY(stage, color, x, y, text) {
+function outTextXY(container, color, x, y, text) {
   // This is the closest approximation to the DOS font and size used in the original Mortar Mayhem
+  var subcontainer = new createjs.Container();
   var text = new createjs.Text(text, "12px TerminalVector", color);
   text.x = x - 1;
   text.y = y;
   text.textBaseline = "hanging";
   text.scaleY = 0.80;
-  stage.addChild(text);
+  subcontainer.addChild(text);
+  container.addChild(subcontainer);
 }
--- menu.js
+++ menu.js
@@ -1,31 +1,29 @@
 function createMenu(stage, assets) {
+    // TODO: Figure out how to make caching (of canvas - for performance) work with text (it resets the font).
+
     var halfScreenWidth = SCREEN_WIDTH / 2;
     var halfScreenHeight = SCREEN_HEIGHT / 2;
 
-    var drawableShape = new createjs.Shape();
-    createBackground();
-    createMainMenu();
+    var backgroundContainer = createBackground();
+    var menuContainer = createMainMenu();
 
-    // We cache the drawable shape to improve performance on static content.
-    drawableShape.cache(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    stage.addChild(drawableShape);
-
+    stage.addChild(backgroundContainer, menuContainer);
+    
     return {
         onTick: onTick
     };
 
     function createBackground() {
-        // TODO: Use a Container for the background image
+        var backgroundContainer = new createjs.Container();
+
         var menutop = new createjs.Bitmap(assets.getResult("menutop.pcx"));
         menutop.x = halfScreenWidth - menutop.image.width / 2;
         menutop.y = halfScreenHeight - menutop.image.height;
-        menutop.cache(0, 0, menutop.image.width, menutop.image.height);
-        stage.addChild(menutop);
+        backgroundContainer.addChild(menutop);
         var menubot = new createjs.Bitmap(assets.getResult("menubot.pcx"));
         menubot.x = halfScreenWidth - menubot.image.width / 2;
         menubot.y = halfScreenHeight;
-        menubot.cache(0, 0, menubot.image.width, menubot.image.height);
-        stage.addChild(menubot);
+        backgroundContainer.addChild(menubot);
 
         var halfMenuWidth = menutop.image.width / 2;
         var halfMenuHeight = (menutop.image.height + menubot.image.height) / 2;
@@ -36,17 +34,22 @@ function createMenu(stage, assets) {
         // SetColor(Gray);
         // Line(GetMaxX div 2 - 319,GetMaxY div 2 - 239,GetMaxX div 2 + 320,GetMaxY div 2 - 239);
         // Line(GetMaxX div 2 - 319,GetMaxY div 2 - 239,GetMaxX div 2 - 319,GetMaxY div 2 + 240);
-        line(drawableShape, Colors.DARKGRAY, halfScreenWidth - halfMenuWidth, halfScreenHeight + halfMenuHeight, halfScreenWidth + halfMenuWidth, halfScreenHeight + halfMenuHeight)
-        line(drawableShape, Colors.DARKGRAY, halfScreenWidth + halfMenuWidth, halfScreenHeight - halfMenuHeight, halfScreenWidth + halfMenuWidth, halfScreenHeight + halfMenuHeight)
-        line(drawableShape, Colors.GRAY, halfScreenWidth - halfMenuWidth, halfScreenHeight - halfMenuHeight, halfScreenWidth + halfMenuWidth, halfScreenHeight - halfMenuHeight)
-        line(drawableShape, Colors.GRAY, halfScreenWidth - halfMenuWidth, halfScreenHeight - halfMenuHeight, halfScreenWidth - halfMenuWidth, halfScreenHeight + halfMenuHeight)
+        var shape = new createjs.Shape();
+        line(shape.graphics, Colors.DARKGRAY, halfScreenWidth - halfMenuWidth, halfScreenHeight + halfMenuHeight, halfScreenWidth + halfMenuWidth, halfScreenHeight + halfMenuHeight)
+        line(shape.graphics, Colors.DARKGRAY, halfScreenWidth + halfMenuWidth, halfScreenHeight - halfMenuHeight, halfScreenWidth + halfMenuWidth, halfScreenHeight + halfMenuHeight)
+        line(shape.graphics, Colors.GRAY, halfScreenWidth - halfMenuWidth, halfScreenHeight - halfMenuHeight, halfScreenWidth + halfMenuWidth, halfScreenHeight - halfMenuHeight)
+        line(shape.graphics, Colors.GRAY, halfScreenWidth - halfMenuWidth, halfScreenHeight - halfMenuHeight, halfScreenWidth - halfMenuWidth, halfScreenHeight + halfMenuHeight)
+        backgroundContainer.addChild(shape);
 
         // SetColor(DarkGray);
         // OutTextXY(GetMaxX div 2 + 286,GetMaxY div 2 + 230,'v'+FloatToStr(Version,0,1));
-        outTextXY(stage, Colors.DARKGRAY, halfScreenWidth + 286, halfScreenHeight + 230, "v1.0");
+        outTextXY(backgroundContainer, Colors.DARKGRAY, halfScreenWidth + 286, halfScreenHeight + 230, "v1.0");
+
+        return backgroundContainer;
     }
 
     function createMainMenu() {
+        var menuContainer = new createjs.Container();
         //  x[1] := GetMaxX div 2 - 80;
         //  y[1] := GetMaxY div 2 - 70;
         //  x[2] := x[1] + 159;
@@ -57,17 +60,22 @@ function createMenu(stage, assets) {
         var y1 = halfScreenHeight - 70;
         var x2 = x1 + 159;
         var y2 = y1 + 193;
-        bar(drawableShape, Colors.DARKGRAY, x1+2,y1+2,x2+2,y2+2);
+        var shape = new createjs.Shape();
+        bar(shape.graphics, Colors.DARKGRAY, x1+2,y1+2,x2+2,y2+2);
+        menuContainer.addChild(shape);
 
         var menuback = new createjs.Bitmap(assets.getResult("menuback.pcx"));
         menuback.x = x1;
         menuback.y = y1;
-        menuback.cache(0, 0, menuback.image.width, menuback.image.height);
-        stage.addChild(menuback);
+        // crop bitmap
+        menuback.sourceRect = new createjs.Rectangle(0, 0, x2 - x1, y2 - y1);
+        menuContainer.addChild(menuback);
 
         //  SetColor(DarkGray);
         //  OutTextXY(GetMaxX div 2 - 68,y[1]+10,'M A I N   M E N U');
-        outTextXY(stage, Colors.DARKGRAY, halfScreenWidth - 68, y1 + 10, "M A I N   M E N U");
+        outTextXY(menuContainer, Colors.DARKGRAY, halfScreenWidth - 68, y1 + 10, "M A I N   M E N U");
+
+        return menuContainer;
     }
 
     function onTick(stage, deltaInSeconds) {
