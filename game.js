f908e0687ece3692da09e2959eec55b75c7f5605
Pelle Stenild Coltau <pelle@tlt.local>
2016-04-02 11:27:24 +0400

    Added setting of player positions and shuffle of players on start.

--- game.js
+++ game.js
@@ -6,7 +6,8 @@ function createGame(onExit, assets) {
 	var noWind = false; // TODO: Get from config (on/off)
 	var wind = 0; // will be set in updateWind()
 	var currentPlayerIndex = 0;
-	var pList = []; // playerList - it's called PList in pascal
+	var pList = []; // playerList - it's called PList in MM
+	var shouldShufflePlayers = true; // FRoundRandom in MM
 
 	var gameGraphics = createGameGraphics(assets, weapons);
 
@@ -25,6 +26,11 @@ function createGame(onExit, assets) {
 		gameGraphics.drawLand(landTop);
 		wind = noWind ? 0 : Math.floor(Math.random() * 41) - 20;
 		gameGraphics.updateWind(wind);
+
+		setPlayerPositions(pList, landTop);
+		if (shouldShufflePlayers) {
+			shufflePlayers(pList);
+		}
 	}
 
 	function generateLand() {
@@ -86,13 +92,47 @@ function createGame(onExit, assets) {
 			weaponList: [{ammo: -1, weaponIndex: 0}],
 			currentWep: 0
 		};
-		/*
-		    if LandTop[PosX] = GetMaxY-18 then
-      PosY := GetMaxY-19
-    else
-      PosY := LandTop[PosX]-1;
-*/
 		return player;
+	}
+
+	function setPlayerPositions(players, landTop) {
+		var averagePlayerSpace = Math.floor(GET_MAX_X / (players.length * 2));
+		var n = 0;
+		var ok = false
+		do {
+			for (var i = 0; i < players.length; ++i) {
+				n = 0;
+				do {
+					ok = true;
+					players[i].posX = Math.floor(Math.random() * (GET_MAX_X - 20)) + 10;
+					for (var x = 0; x < i; ++x) {
+						if (players[i].posX < players[x].posX + averagePlayerSpace && players[i].posX > players[x].posX - averagePlayerSpace) {
+							ok = false;
+						}
+					} 
+					n++;
+				} while (!ok && n < 100);
+			}
+		} while (!ok || n === 100);
+
+		for (var i = 0; i < players.length; ++i) {
+			var posY = landTop[players[i].posX];
+			posY = (posY === GET_MAX_Y - 18) ? posY - 2 : posY - 1;
+			players[i].posY = posY;
+		}
+	}
+
+	function shufflePlayers(players) {
+		for (var i = 0; i < players.length; ++i)	{
+			var m = Math.floor(Math.random() * players.length);
+			var n;
+			do {
+				n = Math.floor(Math.random() * players.length);
+			} while (n === m);
+			var playerMem = players[m];
+			players[m] = players[n];
+			players[n] = playerMem;
+		}
 	}
 
 }
